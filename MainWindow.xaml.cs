0580ac7df22ad2b60821ec93c74ac10af490d0a8
Nikita Baranov <nekit.banan1@mail.ru>
2021-05-12 07:22:04 +0300

    Добавил обработку null

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Windows;
 using System.Windows.Navigation;
 using System.Xml.Linq;
 using TransportRental.Localization;
 
 namespace TransportRental
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
@@ -21,7 +20,18 @@ namespace TransportRental
             if (File.Exists(Environment.CurrentDirectory + "\\settings.cfg")) {
                 var xDocument = XDocument.Load(Environment.CurrentDirectory + "\\settings.cfg");
 
-                LocalizationManager.Instance.CurrentCulture = new CultureInfo(xDocument.Element("Language").Value);
+                var languageValue = xDocument.Element("Language")?.Value;
+
+                if (languageValue == null) {
+                    var result = MessageBox.Show("Ошибка при загрузке языковых настроек!  \r\r\nОбратитесь к системному администратору. \r\r\nValue null", "", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    if (result == MessageBoxResult.OK || result == MessageBoxResult.None)
+                        Process.GetCurrentProcess().Kill();
+
+                    return;
+                }
+
+                LocalizationManager.Instance.CurrentCulture = new CultureInfo(languageValue);
 
                 Source = new Uri("RentPage.xaml", UriKind.Relative);
             }
